a3916bff405333e84e0241a7d703ff702fe20f7b
Jacek Wojtowicz <jacek.wojtowicz@cognitran.com>
2020-04-26 12:50:19 +0200

    pink panther

--- app/src/main/java/com/jw/android_lab/MainActivity.java
+++ app/src/main/java/com/jw/android_lab/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO: to do to do todotodotodo todo todooooooo
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
