c29b3bd1f50a799af295815914f9676c135dd36e
Jacek Wojtowicz <jacek.wojtowicz@cognitran.com>
2020-05-16 15:00:40 +0200

    part2 dummy commit

--- app/src/main/java/com/jw/android_lab/DodajWpis.java
+++ app/src/main/java/com/jw/android_lab/DodajWpis.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import static java.lang.Float.*;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -13,16 +17,24 @@ public class DodajWpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis2);
+        ArrayAdapter gatunki = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, new String[] {"Pies", "Kot", "Rybki"});
+        Spinner gatunek = findViewById(R.id.gatunek);
+        gatunek.setAdapter(gatunki);
+
     }
 
-    public void wyslij (View view) {
-        EditText kontrolka = (EditText) findViewById(R.id.editText2);
-        String pole = kontrolka.getText().toString();
+    public void wyslij (View view)
+    {
+        EditText kolor = findViewById(R.id.kolor);
+        EditText wielkosc = findViewById (R.id.wielkosc);
+        EditText opis = findViewById(R.id.opis);
+        Spinner gatunek = findViewById(R.id.gatunek);
+        Animal zwierze = new Animal(gatunek.getSelectedItem().toString(), kolor.getText().toString(),
+                            valueOf(wielkosc.getText().toString()), opis.getText().toString());
         Intent intencja = new Intent();
-        intencja.putExtra("wpis", pole);
+        intencja.putExtra("nowy", zwierze);
         setResult(RESULT_OK, intencja);
         finish();
     }
-
 
 }
